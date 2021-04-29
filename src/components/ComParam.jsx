0696259db6b200ff9d632bd81557fc5aabdf15cf
Ana Ventura <anaventura1811@gmail.com>
2021-04-29 00:58:24 -0300

    Processamento de dados oriundos de props de componentes

--- src/components/ComParam.jsx
+++ src/components/ComParam.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 
 export default function ComParam(props) {
   const { situacao, aluno, nota } = props;
-  const status = nota >= 7 ? 'Aprovado(a)' : 'Reprovado(a)'
+  const status = nota >= 7 ? 'Aprovado(a)' : 'Reprovado(a)';
+  const notaArredonda = Math.ceil(nota); 
+  // Podemos fazer processamentos de dados antes do componente, 
+  // mas não diretamente sobre as propriedades
+  // É necessário criar outra variável para
+  // utilizar métodos JS, as propriedades/props são 'read-only'
   return (
     <div>
       <h2>{ situacao }</h2>
      <p> 
         <strong>{ aluno } </strong>
         tem nota 
-        <strong> { nota } </strong>
+        <strong> { notaArredonda } </strong>
         e foi <strong> { status }</strong>!
       </p>
     </div>
